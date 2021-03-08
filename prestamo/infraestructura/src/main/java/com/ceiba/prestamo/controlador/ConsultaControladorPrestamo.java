ccbc001221d9afdbb28fbcd9f9bd3e825500d1ac
Hebert Medelo <hebert.medelo@ceiba.com.co>
2021-03-08 13:27:09 -0500

    ajuste conceccion db

--- prestamo/infraestructura/src/main/java/com/ceiba/prestamo/controlador/ConsultaControladorPrestamo.java
+++ prestamo/infraestructura/src/main/java/com/ceiba/prestamo/controlador/ConsultaControladorPrestamo.java
@@ -1,17 +1,11 @@
 package com.ceiba.prestamo.controlador;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
 
 import com.ceiba.prestamo.consulta.ManejadorListarPrestamos;
 import com.ceiba.prestamo.modelo.dto.DtoPrestamo;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
